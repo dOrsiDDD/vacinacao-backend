d54d2a544ac49c038d57f3943a598565fdc4a793
dOrsiDDD <diegodorsi04@gmail.com>
2026-04-18 21:55:47 -0300

    ADD: Implementa interfaces e classes da camada de negócios
    IAgendamentoBusiness, AgendamentoBusiness
    
    IPacienteBusiness, PacienteBusiness
    
    #6

---
+++ AgendamentoVacinacao.Business.Interface/IBusiness/IAgendamentoBusiness.cs
@@ -0,0 +1,16 @@
+﻿using AgendamentoVacinacao.Entities.DTO;
+using AgendamentoVacinacao.Entities.Entities;
+using AgendamentoVacinacao.Entities.Enum;
+
+namespace AgendamentoVacinacao.Business.Interface.IBusiness
+{
+    public interface IAgendamentoBusiness
+    {
+        Task<List<AgendamentoDTO>> ConsultarAgendamentosPorPaciente(int pacienteId);
+        Task<List<AgendamentoDTO>> ListarAgendamentos();
+        Task<List<AgendamentoDTO>> ConsultarAgendamentosPorDia(DateOnly dia);
+        Task<List<AgendamentoDTO>> ConsultarAgendamentosPorHorario(DateTime horario);
+        Task<List<AgendamentoDTO>> FiltrarAgendamentos(StatusEnum status);
+        Task<Agendamento> ObterAgendamentoPorId(int id);
+    }
+}
---
+++ AgendamentoVacinacao.Business.Interface/IBusiness/IPacienteBusiness.cs
@@ -0,0 +1,12 @@
+﻿using AgendamentoVacinacao.Entities.DTO;
+using AgendamentoVacinacao.Entities.Entities;
+
+namespace AgendamentoVacinacao.Business.Interface.IBusiness
+{
+    public interface IPacienteBusiness
+    {
+        Task<List<PacienteDTO>> ListarPacientes();
+        Task<Paciente> ObterPacientePorId(int id);
+        Task<Paciente> ObterPacientePorNome(string nome);
+    }
+}
