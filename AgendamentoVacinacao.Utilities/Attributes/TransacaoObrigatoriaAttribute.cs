5a62119c947da879d5aa14287d95d33ead93e38e
dOrsiDDD <diegodorsi04@gmail.com>
2026-04-24 23:20:20 -0300

    ADD: Implementa gerenciamento de transações
    #20

---
+++ AgendamentoVacinacao.Utilities/Attributes/TransacaoObrigatoriaAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace AgendamentoVacinacao.Utilitarios.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class TransacaoObrigatoriaAttribute : Attribute
+    {
+        public IsolationLevel IsolationLevel { get; set; } = IsolationLevel.ReadCommitted;
+
+        public TransacaoObrigatoriaAttribute() { }
+
+        public TransacaoObrigatoriaAttribute(IsolationLevel isolationLevel)
+        {
+            IsolationLevel = isolationLevel;
+        }
+    }
+}
+
